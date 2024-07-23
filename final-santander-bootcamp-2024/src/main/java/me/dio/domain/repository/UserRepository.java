6ec64c666be2386f7c5e5c167b5115905885dc8a
Mariana Castro <fercastro.mariana@gmail.com>
2024-07-23 12:29:56 -0300

    implementação da camada de domínio

---
+++ final-santander-bootcamp-2024/src/main/java/me/dio/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.dio.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.domain.model.User;
+
+@Repository //na prática não precisa, só de estender JpaRepository o Spring já entende
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
