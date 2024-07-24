68c25d8c35fd053c9a69103bd5f22f6e5c415ead
Mariana Castro <fercastro.mariana@gmail.com>
2024-07-24 00:27:50 -0300

    testing update

--- dio-design-patterns-pokedex/src/main/java/dio/gof/pokemon/DioDesignPatternsPokedexApplication.java
+++ dio-design-patterns-pokedex/src/main/java/dio/gof/pokemon/DioDesignPatternsPokedexApplication.java
@@ -10,6 +10,7 @@ public class DioDesignPatternsPokedexApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DioDesignPatternsPokedexApplication.class, args);
+		
 	}
 
 }
